8b7f46ba51788ee3ac86e747940cba80adcc0219
Michael-Meade <mameade@utica.edu>
2021-02-20 14:32:12 -0500

    the plugin, ip_info will now save stuff to: ips.txt

--- modules/ip_info.rb
+++ modules/ip_info.rb
@@ -1,18 +1,22 @@
 require 'resolv'
+require './lib/reports'
 module SnackHack
     module IpInfo
         def self.get_address(domain)
             Resolv.getaddress(domain)
         end
         def self.get_dns(domain)
+            # GETS IPS 
             Resolv::DNS.open do |dns|
                 ress = dns.getresources domain, Resolv::DNS::Resource::IN::A
-                puts ress.map(&:address)
+                ress.each do |l|
+                    Reports.new(domain, l.address.to_s + "\n", "ips.txt" ).save_txt("a+")
+                end
             end
         end
         def self.run(domain)
-            ip = get_address(domain)
-            p get_dns(domain)
+            ip  = get_address(domain)
+            get_dns(domain)
         end
     end
 end
--- run.rb
+++ run.rb
@@ -1,7 +1,7 @@
 require_relative 'lib/snackhack'
 
-#SnackHack::IpInfo.run("hackex.net")
+SnackHack::IpInfo.run("hackex.net")
 #SnackHack::Robots.run("hackex.net")
 
 #SnackHack::HttpInfo.run("http://cathotel.lu")
-SnackHack::DirBrute.run("http://hackex.net")
+#SnackHack::DirBrute.run("http://hackex.net")
