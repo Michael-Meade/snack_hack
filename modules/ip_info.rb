254498cdd4208e01dfd70bbe21685c8395fbbea9
Michael-Meade <mameade@utica.edu>
2021-02-20 16:23:56 -0500

    added mx support for ip_info module.

--- modules/ip_info.rb
+++ modules/ip_info.rb
@@ -10,13 +10,22 @@ module SnackHack
             Resolv::DNS.open do |dns|
                 ress = dns.getresources domain, Resolv::DNS::Resource::IN::A
                 ress.each do |l|
-                    Reports.new(domain, l.address.to_s + "\n", "ips.txt" ).save_txt("a+")
+                   Reports.new(domain, l.address.to_s + "\n", "ips.txt" ).save_txt("a+")
                 end
+            end
+        end
+        def self.get_mx(domain)
+            # Gets mx 
+            Resolv::DNS.open do |dns|
+                ress = dns.getresources domain, Resolv::DNS::Resource::IN::MX
+                mx   = ress.map { |r| [r.exchange.to_s, r.preference] }
+                Reports.new(domain, mx.to_s + "\n", "mx.txt" ).save_txt
             end
         end
         def self.run(domain)
             ip  = get_address(domain)
             get_dns(domain)
+            get_mx(domain)
         end
     end
 end
