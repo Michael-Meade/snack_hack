49fe9a87ec8caf3b1996a2b4ae772f17af4e87e0
Michael-Meade <mameade@utica.edu>
2021-04-02 11:35:49 -0400

    added comments for robots_score.rb

--- modules/robots_score.rb
+++ modules/robots_score.rb
@@ -8,14 +8,20 @@ module SnackHack
             text     = response.body
             contents = text.split("\n").each { |c|  }.to_a
             contents.each do |l|
+                # makes sure that the line has the 
+                # string "Disallow:" in it
                 if l.include?("Disallow:")
-                    #  remove any empty elements
+                    #  remove any empty elements that are empty
                     deny =  l.split("Disallow: ").reject { |r| r.empty? }
+                    # makes sure that its not already in the array.
                     if !results.include?(deny)
+                        # if is not found in the array
+                        # it will add it to the results array
                         results += deny
                     end
                 end
-            end 
+            end
+        # saves the results into the file named robots_score.txt
         Reports.new(url, results.join("\n"), "robots_score.txt").save_txt
         end
     end
